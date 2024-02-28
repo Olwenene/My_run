4251a23944c6b3608cdf3e1fb6fc8e788fdf9de6
Olwe10 <generalvena2002@gmail.com>
2024-02-28 15:48:16 +0200

    Added all files from sphinx documentation

---
+++ source/conf.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import django
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_run.settings'
+django.setup()
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'my_run'
+copyright = '2024, Olwenene Vena'
+author = 'Olwenene Vena'
+release = '2/28/2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
