353f5d5b4dbce5482feaec0cf51e2c2ff5e45c69
Olwe10 <generalvena2002@gmail.com>
2024-02-28 14:57:51 +0200

    added docstrings to the class in my_run/form.py

--- my_run/form.py
+++ my_run/form.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Question
 
 class Choiceform(forms.ModelForm):
+    """ 
+    A class presenting a form to the user to select their prefered candidate
+    """
     class Meta:
         model = Question
         fields = ['question_text', 'candidate_biography_1', 'candidate_biography_2', 'candidate_biography_3']
